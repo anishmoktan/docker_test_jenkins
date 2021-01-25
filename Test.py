5c8ee12c22801d6f654de92081411a01b3f9f94f
anishmoktan <anish.tmngo@gmail.com>
2021-01-25 00:21:30 -0500

    fail

--- Test.py
+++ Test.py
@@ -11,7 +11,7 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(result,15)
 
     def test_add_2(self):
-        result = 10 + 6
+        result = 10 + 2
         self.assertEqual(result,15)
 
 if __name__ == '__main__':

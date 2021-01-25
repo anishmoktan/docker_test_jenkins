b6ceeac6ed18199156df60af0913b810992a531f
anishmoktan <anish.tmngo@gmail.com>
2021-01-25 00:20:17 -0500

    not pass

--- Test.py
+++ Test.py
@@ -6,12 +6,12 @@ class TestCalc(unittest.TestCase):
         result = 10 + 5
         self.assertEqual(result,15)
 
-    def test_add(self):
+    def test_add_1(self):
         result = 10 + 5
         self.assertEqual(result,15)
 
-    def test_add(self):
-        result = 10 + 5
+    def test_add_2(self):
+        result = 10 + 6
         self.assertEqual(result,15)
 
 if __name__ == '__main__':
